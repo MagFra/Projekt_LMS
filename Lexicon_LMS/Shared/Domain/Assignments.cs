8c0c2a472574ad9d1139f96fd602c7450bcd57d9
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-12-28 10:43:22 +0100

    Created Assignment Class

---
+++ Lexicon_LMS/Shared/Domain/Assignments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Shared.Domain
+{
+    public class Assignments
+    {
+        public string Description { get; set; }
+
+        //Foreign Keys
+        public int ActivityId { get; set; }
+        public int ApplicationUserId { get; set; }
+
+    }
+}
