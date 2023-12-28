ec3182f4d80fd7bbcdf99e821c93948c7e716d18
MagFra <47413425+magfra@users.noreply.github.com>
2023-12-28 13:56:20 +0100

    Uppdaterar DbContext!

--- Lexicon_LMS/Server/Data/ApplicationDbContext.cs
+++ Lexicon_LMS/Server/Data/ApplicationDbContext.cs
@@ -3,11 +3,18 @@ using Lexicon_LMS.Shared.Domain;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Diagnostics;
 
 namespace Lexicon_LMS.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Activity> activity => Set<Activity>();
+        public DbSet<ApplicationUser> applicationUser => Set<ApplicationUser>();
+        public DbSet<Assignments> assignments => Set<Assignments>();
+        public DbSet<Course> course => Set<Course>();
+        public DbSet<Module> module => Set<Module>();
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
