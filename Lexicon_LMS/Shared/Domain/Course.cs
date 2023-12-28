b9d62245fa8ac97ad2fbbfdf527ff11f93d999e3
etnavs <51616907+etnavs@users.noreply.github.com>
2023-12-28 10:15:18 +0100

    Course

---
+++ Lexicon_LMS/Shared/Domain/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.Shared.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public int LengthDays { get; set; }
+        public DateTime LastApplicationDay { get; set; }
+    }
+}
