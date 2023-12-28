74aa954a3df3425dfc128b3c7bbe69939f012bbc
xiahuiliu <coffeeliuxiahui@gmail.com>
2023-12-28 11:08:57 +0000

    Add relationship for course

--- Lexicon_LMS/Shared/Domain/Course.cs
+++ Lexicon_LMS/Shared/Domain/Course.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lexicon_LMS.Shared.Domain;
 
-namespace Lexicon_LMS.Shared.Domain
+public class Course
 {
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; }
-        public int LengthDays { get; set; }
-        public DateTime LastApplicationDay { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public int LengthDays { get; set; }
+    public DateTime LastApplicationDay { get; set; }
 
-    }
+    // Foreign key
+    public int ModuleId { get; set; }
+
+    // Navigation property
+    public ICollection<Module> Modules { get; set; } = new List<Module>();
+    
 }
+
