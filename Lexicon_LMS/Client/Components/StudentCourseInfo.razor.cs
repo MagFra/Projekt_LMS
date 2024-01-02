e9ba3b483dfb483e4f759f8f39200ca826f38a64
Samuel Lidström <samuel.lidstrom@gmail.com>
2024-01-02 13:21:46 +0100

    Created components for Student Portal

--- Lexicon_LMS/Client/Components/StudentCourseInfo.razor.cs
+++ Lexicon_LMS/Client/Components/StudentCourseInfo.razor.cs
@@ -1,6 +1,21 @@
-﻿namespace Lexicon_LMS.Client.Components
+﻿using Lexicon_LMS.Shared.Domain;
+
+namespace Lexicon_LMS.Client.Components
+
 {
     public partial class StudentCourseInfo
     {
+        public Course CourseInfo { get; set; } = new Course();
+
+        public List<Module> ModuleInfo { get; set; } = new List<Module>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ModuleInfo.Add(new Module() { Id = 2, Name="Sjöar", Description="Beskrivning", StartDate= new DateTime(), LengthOfDays=2 });
+
+
+        }
+
     }
 }
