51fe37187ef6ab6d1d23eb858f37afc904e965d8
Samuel Lidström <samuel.lidstrom@gmail.com>
2024-01-02 17:19:40 +0100

    Still not working..

--- Lexicon_LMS/Client/Components/StudentCourseInfo.razor.cs
+++ Lexicon_LMS/Client/Components/StudentCourseInfo.razor.cs
@@ -1,24 +1,19 @@
 ﻿using Lexicon_LMS.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lexicon_LMS.Client.Components
 
 {
     public partial class StudentCourseInfo
     {
-        public CourseDTO CourseInfo { get; set; } = new CourseDTO();
-
-        //public List<ModuleDTO> GetModules()
-        //{
-        //    //return _db
-        //}
-
-        //protected override void OnInitialized()
-        //{
-        //    base.OnInitialized();
-        //    ModuleInfo.Add(new Module() { Id = 2, Name="Sjöar", Description="Beskrivning", StartDate= new DateTime(), LengthOfDays=2 });
+        //public CourseDTO CourseInfo { get; set; } = new CourseDTO();
 
 
-        //}
+
+        public async List<ModuleDTO> ModuleDTOs()
+        {
+            return await //databasen...
+        }
 
     }
 }
