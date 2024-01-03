6e0d3420a3046a6de7b2fbc98ee10e56164cf941
e16johka <e16johka@student.his.se>
2024-01-03 02:42:43 +0100

    AddModule

---
+++ Lexicon_LMS/Client/Components/ModulesData.razor.cs
@@ -0,0 +1,35 @@
+﻿using Lexicon_LMS.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace Lexicon_LMS.Client.Components
+{
+    public partial class ModulesData
+    {
+        public List<ModuleDTO> Modules { get; set; } = new List<ModuleDTO>();
+
+        [Parameter]
+        public string ExtraCaption { get; set; } = string.Empty;
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            //MachineList.Add(new Machine() 
+            //{ MachineID = Guid.NewGuid(), 
+            //  Location = Location.Skövde,
+            //  Date = new DateTime(),
+            //  MachineType = "MachineType1", 
+            //  Status = Status.online 
+            //});
+
+            //MachineList.Add(new Machine()
+            //{
+            //    MachineID = Guid.NewGuid(),
+            //    Location = Location.Göteborg,
+            //    Date = new DateTime(),
+            //    MachineType = "MachineType2",
+            //    Status = Status.offline
+            //});
+
+        }
+    }
+}
+
