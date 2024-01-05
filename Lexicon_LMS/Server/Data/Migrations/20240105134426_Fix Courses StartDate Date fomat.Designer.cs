55832fb92db704fc153a01bb2289cd870b4ee288
MagFra <47413425+magfra@users.noreply.github.com>
2024-01-05 14:45:50 +0100

    Adds a migation for Date format for Starstdate Of Courses

--- Lexicon_LMS/Server/Data/Migrations/20240105134426_Fix Courses StartDate Date fomat.Designer.cs
+++ Lexicon_LMS/Server/Data/Migrations/20240105134426_Fix Courses StartDate Date fomat.Designer.cs
@@ -4,6 +4,7 @@ using Lexicon_LMS.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lexicon_LMS.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240105134426_Fix Courses StartDate Date fomat")]
+    partial class FixCoursesStartDateDatefomat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
