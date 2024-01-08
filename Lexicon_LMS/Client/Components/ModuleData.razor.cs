55ab5d6e1b3b3fc435068bd1d5c0e58a8ada95ca
e16johka <e16johka@student.his.se>
2024-01-08 14:19:04 +0100

    Add module

---
+++ Lexicon_LMS/Client/Components/ModuleData.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Lexicon_LMS.Client.Services;
+using Lexicon_LMS.Shared.Domain.CoursesDTOs;
+using Lexicon_LMS.Shared.Domain.ModulesDTOs;
+
+namespace Lexicon_LMS.Client.Components
+{
+    public partial class ModuleData
+    {
+        [Parameter]
+        [EditorRequired]
+        public string ExtraCaption { get; set; } = string.Empty;
+
+        [Inject]
+        public IModuleService? ModuleService { get; set; }
+
+        public List<ModuleDTO> ModuleLst { get; set; } = new List<ModuleDTO>();
+
+        public string responseData = string.Empty;
+
+        public string ErrorMessage = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+        }
+
+        internal static object FirstOrDefault(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Add(ModuleDTO module)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Lexicon_LMS/Client/Services/IModuleService.cs
@@ -0,0 +1,18 @@
+﻿using Lexicon_LMS.Shared.Domain.CoursesDTOs;
+using Lexicon_LMS.Shared.Domain.ModulesDTOs;
+
+namespace Lexicon_LMS.Server.Services
+{
+    public interface IModuleService
+    {
+        List<ModuleDTO> GetModules();
+
+        CourseDTO GetModule(int Id);
+
+        //void DeleteCourse(int Id);
+
+        //void UpdateCourse(CourseDTO Course);
+
+        void AddModule(ModuleDTO Course);
+    }
+}
