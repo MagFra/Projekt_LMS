d396281a0d884eb621efc9ed9c9a18a1ee2fc520
MagFra <47413425+magfra@users.noreply.github.com>
2024-01-10 11:30:50 +0100

    En fullt körbar version ned mitt senaste "Krångel", misslyckade kod-försök och utkommenterad kod.

---
+++ Lexicon_LMS/Shared/Domain/CoursesDTOs/CourseLimitedDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Lexicon_LMS.Shared.Domain.UsersDTOs;
+
+namespace Lexicon_LMS.Shared.Domain.CoursesDTOs
+{
+    public class CourseLimitedDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public int LengthDays { get; set; }
+    }
+}
