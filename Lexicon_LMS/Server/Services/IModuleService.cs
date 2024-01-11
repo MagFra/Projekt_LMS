642410811d8f2a445907695c7dbf8309561f5a91
MagFra <47413425+magfra@users.noreply.github.com>
2024-01-11 14:38:40 +0100

    Backupp

--- Lexicon_LMS/Server/Services/IModuleService.cs
+++ Lexicon_LMS/Server/Services/IModuleService.cs
@@ -5,7 +5,7 @@ namespace Lexicon_LMS.Server.Services
     public interface IModuleService
     {
         Task<ModuleDTO> AddModuleAsync(ModuleForCreationDTO dto);
-        void DeleteModuleAssync(int moduleId);
+        Task DeleteModuleAssync(int moduleId);
         Task<ModuleDTO> GetModuleAsync(int moduleId);
         Task<IEnumerable<ModuleDTO>> GetModuleListAsync(int? corseId = null);
         Task<bool> UpdateModuleAssync(int id,ModuleForUpdateDTO dto);
