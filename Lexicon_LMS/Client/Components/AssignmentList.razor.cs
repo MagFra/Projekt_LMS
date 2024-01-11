6925ba6827ee2d3d136eb6237cac85587a9d1f92
Xiahui Liu <coffeeliuxiahui@gmail.com>
2024-01-11 16:18:38 +0100

    update assignmentlist

--- Lexicon_LMS/Client/Components/AssignmentList.razor.cs
+++ Lexicon_LMS/Client/Components/AssignmentList.razor.cs
@@ -1,6 +1,43 @@
-﻿namespace Lexicon_LMS.Client.Components
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Lexicon_LMS.Shared.Domain.AssignmetsDTOs;
+
+
+namespace Lexicon_LMS.Client.Components;
+
+public partial class AssignmentList
 {
-    public class Assignment
+    [Inject]
+    private HttpClient? Http { get; set; }
+
+    [Inject]
+    private NavigationManager? NavigationManager { get; set; }
+
+    private List<AssignmentDTO>? assignments;
+
+    private string? ErrorMessage;
+
+
+    protected override async Task OnInitializedAsync()
     {
+        try
+        {
+            var response = await Http!.GetFromJsonAsync<List<AssignmentDTO>>("/api/activities");
+
+            if (response != null)
+            {
+                assignments = response;
+            }
+            else
+            {
+                ErrorMessage = "Could not read assignments from API!";
+            }
+        }
+        catch (Exception exception)
+        {
+            ErrorMessage = exception.Message;
+        }
     }
 }
+
+
